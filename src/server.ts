68fa532a1296ef35cf62b4e8b549849517859ac0
Giovanne Piçuti <picuti256@gmail.com>
2024-04-03 16:49:03 -0300

    Prisma first config

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import fastify from "fastify";
 
 const app = fastify();
 
+app.post("/events", (req, res) => {
+  console.log(req.body);
+  return "Hello World!";
+});
+
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running on port 3333");
 });
