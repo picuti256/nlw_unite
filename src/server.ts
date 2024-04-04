061867c3b841341b0aa0e5139b50da4771d9f8d5
Giovanne Piçuti <picuti256@gmail.com>
2024-04-03 23:06:35 -0300

    create routes

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,16 @@
 import fastify from "fastify";
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
 
 const app = fastify();
 
-const prisma = new PrismaClient({
-  log: ["query"],
-});
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.post("/events", async (req, res) => {
-  const createdEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable(),
-  });
-
-  const data = createdEventSchema.parse(req.body);
-
-  const event = await prisma.event.create({
-    data: {
-      title: data.title,
-      details: data.details,
-      maximumAttendees: data.maximumAttendees,
-      slug: new Date().toISOString(),
-    },
-  });
-
-  console.log(req.body);
-  return res.status(201).send({ eventId: event.id });
-});
+app.register(createEvent);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running on port 3333");
