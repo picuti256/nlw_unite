c053715fc82a6df34810d19642b6fe26d7487b20
Giovanne Piçuti <picuti256@gmail.com>
2024-04-11 23:33:12 -0300

    error handler config

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { BadRequest } from "./_errors/bad-request";
 
 export async function registerForEvent(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -38,7 +39,9 @@ export async function registerForEvent(app: FastifyInstance) {
       });
 
       if (attendeeFromEmail !== null) {
-        throw new Error("This attendee is already register for this event.");
+        throw new BadRequest(
+          "This attendee is already register for this event."
+        );
       }
 
       const [event, amountOfAttendeesForEvent] = await Promise.all([
@@ -59,7 +62,7 @@ export async function registerForEvent(app: FastifyInstance) {
         event?.maximumAttendees &&
         amountOfAttendeesForEvent >= event?.maximumAttendees
       ) {
-        throw new Error(
+        throw new BadRequest(
           "The maximum number of attendees for this event has been reached."
         );
       }
--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
+import { errorHandler } from "./error-handler";
 
 const app = fastify();
 
@@ -44,6 +45,8 @@ app.register(getEvent);
 app.register(getAttendeeBadge);
 app.register(checkIn);
 app.register(getEventAttendees);
+
+app.setErrorHandler(errorHandler);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running on port 3333");
