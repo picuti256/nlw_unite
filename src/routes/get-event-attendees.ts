59dd040458757bc2c31d7d563617ddf3ad88e9ab
Giovanne Piçuti <picuti256@gmail.com>
2024-04-11 22:00:16 -0300

    Swagger config

--- src/routes/get-event-attendees.ts
+++ src/routes/get-event-attendees.ts
@@ -8,7 +8,7 @@ export async function getEventAttendees(app: FastifyInstance) {
     "/events/:eventId/attendees",
     {
       schema: {
-        summary: "Get an event",
+        summary: "Get event attendees",
         tags: ["events"],
         params: z.object({
           eventId: z.string().uuid(),
